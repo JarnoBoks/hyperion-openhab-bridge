372c0e6cca494a451ee3dc15595147a74a2f3d70
root <root@HyperBian>
2025-10-31 11:39:07 +0100

    Systemd fixes on RPi

--- config.js
+++ config.js
@@ -1,9 +1,5 @@
 module.exports = {
   lights: [
     { id: "TST_Zigbee__Hue_RGB_Bulb_04_HA_Discovery_Color_rgb_string", type: "rgb" },
-    {
-      id: "ikea_of_sweden_tradfribulbe27wsglobeopal1055lm_light",
-      type: "dim",
-    },
   ],
 };
